d9fc8fa7b3d9f9b82336b47d2753d4ca8abf77f7
Victor Brenister <victorbrenister@vandebron.nl>
2026-04-23 18:59:44 +0300

    Fixed delete button type error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,13 +69,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('SOmethign went wrong');
-    try {
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
-    } catch (err) {
-        console.log(err);
-        return { message: 'Database Error: Failed to Delete Invoice.' };
-    }
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
 
     revalidatePath('/dashboard/invoices');
 }
